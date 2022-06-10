715430e6f7c36bc45066eb3f2b0152974196332c
518H0090 <nhoxhieuro5@gmail.com>
2022-06-10 18:53:46 +0700

    InitialCreate

---
+++ BasicCRUD/backend/WebApi/WebApi/Data/DonationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApi.Models;
+
+namespace WebApi.Data
+{
+    public class DonationDbContext : DbContext
+    {
+        public DonationDbContext(DbContextOptions<DonationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<DCandidate> dCandidates { get; set; }
+
+    }
+}
---
+++ BasicCRUD/backend/WebApi/WebApi/Migrations/20220610111228_InitialCreate.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApi.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "dCandidates",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    fullName = table.Column<string>(type: "nvarchar(100)", nullable: false),
+                    mobile = table.Column<string>(type: "nvarchar(16)", nullable: false),
+                    email = table.Column<string>(type: "nvarchar(100)", nullable: false),
+                    age = table.Column<int>(type: "int", nullable: false),
+                    bloodGroup = table.Column<string>(type: "nvarchar(3)", nullable: false),
+                    MyProperty = table.Column<string>(type: "nvarchar(100)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_dCandidates", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "dCandidates");
+        }
+    }
+}
