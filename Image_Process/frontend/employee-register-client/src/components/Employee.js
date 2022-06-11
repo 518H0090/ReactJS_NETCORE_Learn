28905cc0d19fb05bbeece6f365ecf60fc040130d
518H0090 <nhoxhieuro5@gmail.com>
2022-06-11 10:58:21 +0700

    Logic Create

--- Image_Process/frontend/employee-register-client/src/components/Employee.js
+++ Image_Process/frontend/employee-register-client/src/components/Employee.js
@@ -11,8 +11,11 @@ const initialFieldValues = {
   imageFile: null,
 };
 
-export default function Employee() {
+export default function Employee(props) {
+  const { addOrEdit } = props;
+
   const [values, setValues] = useState(initialFieldValues);
+  const [errors, setErrors] = useState({});
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
@@ -43,12 +46,44 @@ export default function Employee() {
     }
   };
 
+  const validate = (obj) => {
+    let temp = {};
+    temp.employeeName = values.employeeName == "" ? false : true;
+    temp.imageSrc = values.imageSrc == defaultImageSrc ? false : true;
+    setErrors(temp);
+    return Object.values(temp).every((x) => x === true);
+  };
+
+  const resetForm = () => {
+    setValues(initialFieldValues);
+    document.getElementById("image-uploader").value = null;
+    setErrors({});
+  };
+
+  //   SubmitForm
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    if (validate()) {
+      const formData = new FormData();
+      formData.append("EmployeeId", values.employeeId);
+      formData.append("EmployeeName", values.employeeName);
+      formData.append("occupation", values.occupation);
+      formData.append("imageName", values.imageName);
+      formData.append("imageFile", values.imageFile);
+
+      addOrEdit(formData, resetForm);
+    }
+  };
+
+  const applyErrorClass = (field) =>
+    field in errors && errors[field] === false ? "invalid-field" : "";
+
   return (
     <div>
       <div className="container text-center">
         <p className="lead">An Employee</p>
       </div>
-      <form autoComplete="off" noValidate>
+      <form autoComplete="off" noValidate onSubmit={handleFormSubmit}>
         <div className="card">
           <img
             src={values.imageSrc}
@@ -61,14 +96,15 @@ export default function Employee() {
               <input
                 type="file"
                 accept="image/*"
-                className="form-control-file"
+                className={"form-control-file " + applyErrorClass("imageSrc")}
                 onChange={showPreview}
+                id="image-uploader"
               />
             </div>
 
             <div className="form-group">
               <input
-                className="form-control"
+                className={"form-control " + applyErrorClass("employeeName")}
                 placeholder="Employee Name"
                 name="employeeName"
                 value={values.employeeName}
@@ -83,6 +119,11 @@ export default function Employee() {
                 value={values.occupation}
                 onChange={handleInputChange}
               />
+            </div>
+            <div className="form-group text-center">
+              <button type="submit" className="btn btn-light">
+                submit
+              </button>
             </div>
           </div>
         </div>
--- Image_Process/frontend/employee-register-client/src/components/EmployeeList.js
+++ Image_Process/frontend/employee-register-client/src/components/EmployeeList.js
@@ -1,18 +1,39 @@
 import React from "react";
 import Employee from "./Employee";
+import axios from "axios";
 
 export default function EmployeeList() {
+  const employeeAPI = (url = "") => {
+    return {
+      fetchAll: () => axios.get(url),
+      create: (newRecord) => axios.post(url, newRecord),
+      update: (id, updateRecord) => axios.put(url + id, updateRecord),
+      delete: (id) => axios.delete(url + id),
+    };
+  };
+
+  const addOrEdit = (formData, onSuccess) => {
+    employeeAPI()
+      .create(formData)
+      .then((res) => {
+        onSuccess();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <div className="row">
       <div className="col-md-12">
-        <div class="mt-4 p-5 py-4 bg-primary text-white rounded">
+        <div className="mt-4 p-5 py-4 bg-primary text-white rounded">
           <div className="container text-center">
             <h1 className="display-4">Employee Register</h1>
           </div>
         </div>
       </div>
       <div className="col-md-4">
-        <Employee />
+        <Employee addOrEdit={addOrEdit} />
       </div>
       <div className="col-md-4">
         <div>List of Employee</div>
